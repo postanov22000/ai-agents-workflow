cdd5ce9c4af9d4c0e5674c3ac1543125f150df22
optirank <siradjads@gmail.com>
2025-04-24 21:49:55 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,11 +1,13 @@
+import os
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 def authenticate():
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    creds = flow.run_local_server(port=0)
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
+    creds = Credentials.from_authorized_user_info({
+        "client_id": os.getenv("GMAIL_CLIENT_ID"),
+        "client_secret": os.getenv("GMAIL_CLIENT_SECRET"),
+        "refresh_token": os.getenv("GMAIL_REFRESH_TOKEN")
+    }, SCOPES)
     return creds
