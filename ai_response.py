db771ffe41d9600e44104694865e53b7a7d77bcc
optirank <siradjads@gmail.com>
2025-04-24 22:43:32 +0200

    Update ai_response.py

--- ai_response.py
+++ ai_response.py
@@ -1,3 +1,6 @@
+import os  # Add this line at the very top
+from nltk.sentiment import SentimentIntensityAnalyzer
+import spacy
 if os.getenv("GITHUB_ACTIONS") == "true":
     respond_to_emails()  # Run once
 else:
