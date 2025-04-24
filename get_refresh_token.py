a57a94de102964bd58872c40c34eaf531c64d944
optirank <siradjads@gmail.com>
2025-04-24 23:27:21 +0200

    Update get_refresh_token.py

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -5,6 +5,8 @@ flow = InstalledAppFlow.from_client_secrets_file(
     scopes=['https://www.googleapis.com/auth/gmail.modify']
 )
 creds = flow.run_local_server(port=0)
-with open('token.json', 'w') as token:
-    token.write(creds.to_json())
+print('Copy ALL content between === lines:')
+print('=== START TOKEN ===')
+print(creds.to_json())
+print('=== END TOKEN ===')
 "
