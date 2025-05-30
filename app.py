f20b69b338db7941685802da9921cdbcceb40309
optirank <siradjads@gmail.com>
2025-05-30 11:09:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -173,6 +173,7 @@ def oauth2callback():
         token_payload = {
             "user_id": user_id,
             "credentials": {
+                "user_email": user_email,
                 "token": credentials.token,
                 "refresh_token": credentials.refresh_token,
                 "token_uri": credentials.token_uri,
