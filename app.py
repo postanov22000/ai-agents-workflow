a731586f1f5a00b2dfc24baebc84656aa2f388ba
optirank <siradjads@gmail.com>
2025-06-09 15:24:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -261,6 +261,7 @@ def oauth2callback():
                 "https://www.googleapis.com/auth/gmail.send",
                 "https://www.googleapis.com/auth/gmail.readonly",
                 "https://www.googleapis.com/auth/userinfo.email",
+                "https://www.googleapis.com/auth/gmail.compose", 
                 "openid"
             ]
         )
